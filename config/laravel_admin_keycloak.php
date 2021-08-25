e846a4cbca1ed9024b2d9e938bd54659f033b423
Andrey Toropin <at@rusatom.dev>
2021-08-25 20:14:43 +0300

    fix

--- config/laravel_admin_keycloak.php
+++ config/laravel_admin_keycloak.php
@@ -5,6 +5,6 @@ return [
     'client_id'     => env('KEYCLOAK_CLIENT_ID'),
     'client_secret' => env('KEYCLOAK_CLIENT_SECRET'),
     'base_url'      => env('KEYCLOAK_BASE_URL'),
-    'redirect'      => '${APP_URL}/admin/login/keycloak_admin/callback'
+    'redirect'      => env('APP_URL').'/admin/login/keycloak/callback'
 
 ];
