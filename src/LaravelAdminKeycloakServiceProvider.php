288fa5f3ab0958bcd2d0452fadad6110a6420033
Andrey Toropin <at@rusatom.dev>
2021-08-26 14:05:53 +0300

    te

--- src/LaravelAdminKeycloakServiceProvider.php
+++ src/LaravelAdminKeycloakServiceProvider.php
@@ -59,7 +59,7 @@ class LaravelAdminKeycloakServiceProvider extends ServiceProvider
         $socialite->extend(
             'la_keycloak',
             function ($app) use ($socialite) {
-                $config = $app['config']['la_keycloak'];
+                $config = $app['config']['laravel_admin_keycloak'];
                 $configClass = new SocialiteConfig(
                     $app['config']['laravel_admin_keycloak.client_id'],
                     $app['config']['laravel_admin_keycloak.client_secret'],
